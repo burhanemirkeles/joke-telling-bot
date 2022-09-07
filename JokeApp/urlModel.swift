fbf8fa6fe72eb9532812c965c6b113f207f4940f
emirkeles <burhanemirkeles@outlook.com>
2022-09-07 15:17:25 +0300

    apikey change

--- JokeApp/urlModel.swift
+++ JokeApp/urlModel.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct UrlModel{
-    let urlString = "https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&idRange=0-150&flags=racist"
+    let urlString = "https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&idRange=0-150&flags=nsfw,racist,sexist,explicit"
     let headers = [
-        "X-RapidAPI-Key"    :   "26d94ab478msh43188b1a7087989p1d6af0jsna19a1e7f5171",
+        "X-RapidAPI-Key"    :   apiKeyModel.init().apiKey,
         "X-RapidAPI-Host"   :   "jokeapi-v2.p.rapidapi.com"
     ]
 }
