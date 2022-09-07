de37df9888494e9016c615175b5e77ccaac869c9
emirkeles <burhanemirkeles@outlook.com>
2022-09-07 15:21:05 +0300

    apiKey change

---
+++ JokeApp/apiKeyModel.swift
@@ -0,0 +1,12 @@
+//
+//  apiKeyModel.swift
+//  JokeApp
+//
+//  Created by Emir Keles on 7.09.2022.
+//
+
+import Foundation
+
+struct ApiKeyModel{
+    let apiKey = "26d94ab478msh43188b1a7087989p1d6af0jsna19a1e7f5171"
+}
--- JokeApp/urlModel.swift
+++ JokeApp/urlModel.swift
@@ -10,7 +10,7 @@ import Foundation
 struct UrlModel{
     let urlString = "https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&idRange=0-150&flags=nsfw,racist,sexist,explicit"
     let headers = [
-        "X-RapidAPI-Key"    :   apiKeyModel.init().apiKey,
+        "X-RapidAPI-Key"    :   ApiKeyModel.init().apiKey,
         "X-RapidAPI-Host"   :   "jokeapi-v2.p.rapidapi.com"
     ]
 }
